1f056e472661e236e493c06f7782edd98f1a4ba9
Arindam Pani <arindampani@gmail.com>
2020-01-27 14:27:54 -0700

    updated tile map

---
+++ Ex3/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public float movementSpeed = 5.0f;
+
+    public Rigidbody2D rigidBody;
+
+    private Vector2 _movement;
+    // Update is called once per frame
+    private void Update()
+    {
+        _movement.x = Input.GetAxis("Horizontal");
+        _movement.y = Input.GetAxis("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+        rigidBody.MovePosition(rigidBody.position + _movement * movementSpeed * Time.fixedDeltaTime);
+    }
+}
