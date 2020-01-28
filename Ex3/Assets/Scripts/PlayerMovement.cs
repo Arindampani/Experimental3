64520595088ad92a40bebf21edfb40433d9c08dd
Arindam Pani <arindampani@gmail.com>
2020-01-28 01:56:31 -0700

    added tile rules

--- Ex3/Assets/Scripts/PlayerMovement.cs
+++ Ex3/Assets/Scripts/PlayerMovement.cs
@@ -13,18 +13,17 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     private void Update()
     {
-
-
-
-
-
-
         _movement.x = Input.GetAxis("Horizontal");
         _movement.y = Input.GetAxis("Vertical");
     }
 
     private void FixedUpdate()
     {
-        rigidBody.MovePosition(rigidBody.position + _movement * movementSpeed * Time.fixedDeltaTime);
+        //rigidBody.MovePosition(rigidBody.position + _movement * movementSpeed * Time.fixedDeltaTime);
+    }
+
+    public void Move()
+    {
+
     }
 }
