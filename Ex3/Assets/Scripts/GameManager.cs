c6710e9759fbcd7e1cf086fcb82725f83f4153ae
Arindam Pani <arindampani@gmail.com>
2020-01-28 09:31:19 -0700

    almost final changes

--- Ex3/Assets/Scripts/GameManager.cs
+++ Ex3/Assets/Scripts/GameManager.cs
@@ -16,45 +16,61 @@ public class GameManager : MonoBehaviour
     public Transform playerTransform;
 
     private int diceValue;
+
+    private float xValue;
+    private float yValue;
     bool movementDone = true;
     // Start is called before the first frame update
     void Start()
     {
         float vertExtent = Camera.main.orthographicSize;
         float horzExtent = vertExtent * Screen.width / Screen.height;
+
+        if (movementDone)
+        {
+            diceValue = Random.Range(1, 6);
+            Debug.Log(diceValue);
+            dieValueText.text = diceValue.ToString();
+            movementDone = false;
+        }
+        xValue = playerTransform.position.x;
+        yValue = playerTransform.position.y;
     }
 
     // Update is called once per frame
     void Update()
     {
+
+
+            if ((Input.GetAxisRaw("Horizontal") != 0.0f || Input.GetAxisRaw("Vertical") != 0.0f ) && !movementDone)
+            {
+
+                for (int i = 0; i < diceValue; i++)
+                {
+                    //Debug.Log(movementDone);
+                    Debug.Log(diceValue);
+                //Debug.Log(i);
+                //Debug.Log(Input.GetAxisRaw("Vertical"));
+                Debug.Log(new Vector3Int(Mathf.FloorToInt(playerTransform.position.x + 1 * Input.GetAxisRaw("Horizontal") * 0.8f), Mathf.FloorToInt(playerTransform.position.y + 1 * Input.GetAxisRaw("Vertical") * 0.8f), 0));
+                    tilemap.SetTile(
+                        new Vector3Int(Mathf.FloorToInt(playerTransform.position.x +1* Input.GetAxisRaw("Horizontal") * 0.8f ),Mathf.FloorToInt(playerTransform.position.y + 1*Input.GetAxisRaw("Vertical") * 0.8f),0),
+                    null);
+                playerTransform.position = new Vector3(playerTransform.position.x + 1 * Input.GetAxisRaw("Horizontal"), playerTransform.position.y + 1 * Input.GetAxisRaw("Vertical"), 0);
+            }
+            
+            //playerTransform.position = new Vector3(playerTransform.position.x + diceValue*Input.GetAxisRaw("Horizontal") * Time.deltaTime, playerTransform.position.y + diceValue*Input.GetAxisRaw("Vertical") * Time.deltaTime, 0);
+            movementDone = true;
+            resetDice();
+        }
+    }
+
+    void resetDice()
+    {
         if (movementDone)
         {
             diceValue = Random.Range(1, 6);
-
             dieValueText.text = diceValue.ToString();
             movementDone = false;
         }
-        else
-        {
-            if (Input.GetAxisRaw("Horizontal") != 0.0f || Input.GetAxisRaw("Vertical") != 0.0f)
-            {
-               
-                for (int i = 0; i < diceValue; i++)
-                {
-                    Debug.Log(movementDone);
-                    Debug.Log(diceValue);
-                    Debug.Log(i);
-                    Debug.Log(Input.GetAxisRaw("Vertical"));
-                    tilemap.SetTile(
-                        new Vector3Int(Mathf.FloorToInt(playerTransform.position.x +i* Input.GetAxisRaw("Horizontal")),Mathf.FloorToInt(playerTransform.position.y + i*Input.GetAxisRaw("Vertical")),0),
-                    null);
-                    playerTransform.position = new Vector3(playerTransform.position.x + 1 * Input.GetAxisRaw("Horizontal") , playerTransform.position.y + 1 * Input.GetAxisRaw("Vertical"), 0);
-                }
-                //playerTransform.position = new Vector3(playerTransform.position.x + diceValue*Input.GetAxisRaw("Horizontal") * Time.deltaTime, playerTransform.position.y + diceValue*Input.GetAxisRaw("Vertical") * Time.deltaTime, 0);
-                movementDone = true;
-            }
-
-        }
-        
     }
 }
